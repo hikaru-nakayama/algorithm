be66694eec028c1df20bf9eaf5169455be354eb7
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-04-04 00:07:45 +0900

    add Nil value if not exist!

--- tree/rooted_tree.go
+++ tree/rooted_tree.go
@@ -23,6 +23,7 @@ func (n *Node) String(id int) {
 }
 
 func Start() {
+	var Nil = "-1"
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	input := scanner.Text()
@@ -33,19 +34,29 @@ func Start() {
 	tree := Tree{
 		Nodes: make([]Node, totalNodeNum),
 	}
+	for i := range tree.Nodes {
+		tree.Nodes[i].Parent = "-1"
+		tree.Nodes[i].Right = "-1"
+	}
 	for i := 0; i < totalNodeNum; i++ {
 		scanner.Scan()
 		inputs := strings.Split(scanner.Text(), " ")
-		childs := inputs[1:]
+		if inputs[1] == "0" {
+			tree.Nodes[i].Left = Nil
+			continue
+		}
+		childs := inputs[2:]
 		tree.Nodes[i].Left = childs[0]
-		for _, c := range childs {
+		for j, c := range childs {
 			ch, err := strconv.Atoi(c)
 			if err != nil {
 				fmt.Println("数字を入力せよ")
 			}
 			tree.Nodes[ch].Parent = strconv.Itoa(i)
-			if ch+1 < len(childs) {
-				tree.Nodes[ch].Right = childs[ch+1]
+			if j+1 < len(childs) {
+				tree.Nodes[ch].Right = childs[j+1]
+			} else {
+				tree.Nodes[ch].Right = Nil
 			}
 		}
 	}
