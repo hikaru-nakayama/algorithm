44e034ff23994420e41204d118f01411fab06eef
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-04-03 22:52:53 +0900

    enable to scan

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	tree.Hello()
+	tree.Start()
 }
--- tree/rooted_tree.go
+++ tree/rooted_tree.go
@@ -1,9 +1,27 @@
 package tree
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func Hello() {
-	fmt.Println("Hello World!")
+type Node struct {
+	Parent int64 // ノードの親
+	Left   int64 // ノードの最も左の子
+	Right  int64 // ノードのすぐ右の兄弟
+}
+
+type Tree struct {
+	Nodes []Node
+}
+
+func Start() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	inputs := strings.Split(scanner.Text(), " ")
+	for _, input := range inputs {
+		fmt.Println(input)
+	}
 }
