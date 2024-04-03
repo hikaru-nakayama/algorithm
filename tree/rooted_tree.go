ce393c0f41015eb6fe8eeda2e5ddae5248d283b3
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-04-03 23:37:34 +0900

    Build tree

--- tree/rooted_tree.go
+++ tree/rooted_tree.go
@@ -4,24 +4,52 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Node struct {
-	Parent int64 // ノードの親
-	Left   int64 // ノードの最も左の子
-	Right  int64 // ノードのすぐ右の兄弟
+	Parent string // ノードの親
+	Left   string // ノードの最も左の子
+	Right  string // ノードのすぐ右の兄弟
 }
 
 type Tree struct {
 	Nodes []Node
 }
 
+func (n *Node) String(id int) {
+	fmt.Printf("Self: %d Parent: %s Left %s Right %s\n", id, n.Parent, n.Left, n.Right)
+}
+
 func Start() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	inputs := strings.Split(scanner.Text(), " ")
-	for _, input := range inputs {
-		fmt.Println(input)
+	input := scanner.Text()
+	totalNodeNum, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("数字を入力せよ")
+	}
+	tree := Tree{
+		Nodes: make([]Node, totalNodeNum),
+	}
+	for i := 0; i < totalNodeNum; i++ {
+		scanner.Scan()
+		inputs := strings.Split(scanner.Text(), " ")
+		childs := inputs[1:]
+		tree.Nodes[i].Left = childs[0]
+		for _, c := range childs {
+			ch, err := strconv.Atoi(c)
+			if err != nil {
+				fmt.Println("数字を入力せよ")
+			}
+			tree.Nodes[ch].Parent = strconv.Itoa(i)
+			if ch+1 < len(childs) {
+				tree.Nodes[ch].Right = childs[ch+1]
+			}
+		}
+	}
+	for i, n := range tree.Nodes {
+		n.String(i)
 	}
 }
